a19549a52af4285e243cd6c5cc4bfa4ca3e9a3ba
Natalie Huang <natalie.huang99@gmail.com>
2020-07-14 11:13:07 -0700

    update to display emotion data

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 function handle(event){
+
     event.stopPropagation();
     event.preventDefault();
     console.log('---- handle ---');
 
+
 var myform = document.getElementById('myform');
+
     console.log('---- myform ---'+myform);
-
     var payload = new FormData(myform);
-
     console.log('---- handle completed---');
 
     $.ajax({
@@ -15,15 +16,43 @@ var myform = document.getElementById('myform');
         url: "https://songrecapp.azurewebsites.net/api/SongRecTrigger",
         crossDomain: true,
         data: payload,
-    contentType: false,
-    processData: false,
+        contentType: false,
+        processData: false,
+
         success: function (resp) {
             console.log(resp);
             var data = JSON.parse(resp);
-            $("#responseTextArea").val(data.emotion);
+            console.log(data.result);
+
+            data = data.result[0];
+            console.log(data);
+            var emotion = data.faceAttributes.emotion;
+
+            var resultString = `
+
+            <h3>Emotions in the image:</h3><br />
+            <p>anger: ${emotion.anger}</p>
+            <p>contempt: ${emotion.contempt}</p>
+            <p>disgust: ${emotion.disgust}</p>
+            <p>fear: ${emotion.fear}</p>
+            <p>happiness: ${emotion.happiness}</p>
+            <p>neutral: ${emotion.neutral}</p>
+            <p>sadness: ${emotion.sadness}</p>
+            <p>surprise: ${emotion.surprise}</p>
+            `;
+
+            $('#emotion').html(resultString);
         },
+
         error: function (err) {
             console.log('---ERROR: '+err);
         }
     });
+}
+
+
+function clearForm(){
+    document.getElementById('myform').reset();
+    document.getElementById('emotion').innerHTML = '';
+
 }
